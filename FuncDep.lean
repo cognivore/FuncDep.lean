3cb617ab8abc3d309784be76b70a4a601b8a99f5
cognivore <noreply>
2022-07-07 14:29:07 +0100

    Problem: Unclear if useful funcdeps are impossible
    Solution:
    
     - Demonstrate that very likely they are impossible to implement in Lean 4.
    
    Indeed, there is no way to deduce CountParts_ S from CoutnParts S \m_0 \a_0!
    
    Notably, the errors are always at the caller's site, FuncDep.lean would typecheck no matter what code the users write, which is fair[1].
    
    Some day, however, I'd be interested in seeing what neat tricks we can do with Lean 4 typeclasses, aspiring to some cool abuses of the system[2], but for the time being, we advise to treat typeclasses in Lean 4 as nothing more than multiparameter traits!
    Speaking of which, I'll test the applicability of typeclasses for multiparameter traiting in the upcoming patch.
    
    [1]: https://serokell.io/blog/learn-from-errors-overlapping-instances
    [2]: https://lexi-lambda.github.io/blog/2021/03/25/an-introduction-to-typeclass-metaprogramming/

--- FuncDep.lean
+++ FuncDep.lean
@@ -21,5 +21,24 @@ instance (i : CountParts S μₒ αₒ) : CountParts_ S where
 instance : CountParts String Float Char where
   φ _ _ := '?'
 
+/- Somehow, Lean manages to see `Nat ~ CountParts_.α String` in the instance declaration! -/
 instance : CountParts String Char Nat where
-  φ haystack needle := haystack.foldl (fun acc x => acc + if x == needle then 1 else 0) 0
+  φ haystack needle := FuncDep.instCountParts_String.φ haystack needle
+
+instance : CountParts Char Float Char where
+  φ _ _ := '!'
+
+-- instance : CountParts Char Nat Bool where
+--   φ _ _ := true
+
+
+/-
+type mismatch
+  CountParts_.φ haystack needle
+has type
+  CountParts_.α String : Type
+but is expected to have type
+  Float : Type
+-/
+-- instance : CountParts String Char Float where
+--   φ haystack needle := FuncDep.instCountParts_String.φ haystack needle
--- Main.lean
+++ Main.lean
@@ -6,10 +6,48 @@ def countStrX (xs : String) : Nat :=
 def countNonsense (xs : String) : Char :=
   FuncDep.CountParts.φ xs 0.3
 
+def concreteInstanceCall (xs : String) : FuncDep.instCountParts_String.α  :=
+  FuncDep.CountParts_.φ xs 'x'
+
+def deducedInstanceCall (xs : T) [i : FuncDep.CountParts_ T] :=
+  FuncDep.CountParts_.φ xs
+
+/-
+failed to synthesize instance
+  FuncDep.CountParts_ Char
+-/
+-- def concreteDeducedInstanceCall (x : Char) := deducedInstanceCall x
+
+/-
+application type mismatch
+  deducedInstanceCall x y
+argument
+  y
+has type
+  Float : Type
+but is expected to have type
+  FuncDep.CountParts_.μ Char : Type ?u.341
+-/
+-- def concreteDeducedInstanceCall' (x : Char) (y : Float) : Char := deducedInstanceCall x y
+
+/-
+application type mismatch
+  deducedInstanceCall x y
+argument
+  y
+has type
+  T1 : Type ?u.354
+but is expected to have type
+  FuncDep.CountParts_.μ Char : Type ?u.367
+-/
+-- def concreteDeducedInstanceCall'' (x : Char) [i : FuncDep.CountParts Char T1 T2] (y : T1) : T2 := deducedInstanceCall x y
+
 def main : IO Unit := do
   let nₓ := countStrX "Rhox"
   IO.println s!"{nₓ}"
   -- let n₂ := FuncDep.CountParts.φ "Tarmogoyf" 'o'
   -- IO.println s!"{n₂}"
-  let n₃ := countNonsense "Paradox Engine"
+  let n₃ : Nat := FuncDep.CountParts.φ "Tarmogoyf" 'o'
   IO.println s!"{n₃}"
+  let n₄ := countNonsense "Paradox Engine"
+  IO.println s!"{n₄}"
