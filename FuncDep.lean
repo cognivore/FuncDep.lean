ad0188c88f6b2faa9c764d6eaa565b331fb91637
cognivore <noreply>
2022-07-07 14:03:50 +0100

    Problem: Unclear if types-as-methods are useless
    Solution:
    
     - Demonstrate that they are.
     - First of all, FuncDep.lean module is contradicting. The moment someone will try to derive CountParts_ for String, the compilation will fail (I'll check that in an upcoming patch), because the instances defined in FuncDep.lean are heavily contradicting or duplicating.
     - Sadly, simply using nonsensical instances doesn't cause the compiler to generate instance on line 16 of FuncDep.lean, so it doesn't error out.
     - Even though we technically have just one instances defined for type tuple (String, Char), we still need to tell the compiler what do we want to get out as the result. I think, it's because instances are ambiguous by default?.. I'll test it in the next patch too.

--- FuncDep.lean
+++ FuncDep.lean
@@ -1,12 +1,25 @@
 namespace FuncDep
 
-class CountParts (S : Type u) where
+class CountParts_ (S : Type u) where
   μ : Type v
   α : Type w
   φ : S → μ → α
 
-instance : CountParts String where
+instance : CountParts_ String where
   μ := Char
   α := Nat
   φ haystack needle := haystack.foldl (fun acc x => acc + if x == needle then 1 else 0) 0
-  -- φ (needle : Char) (haystack : String) := haystack.
+
+class CountParts (S : Type u) (μ : Type v) (α : Type w) where
+  φ : S → μ → α
+
+instance (i : CountParts S μₒ αₒ) : CountParts_ S where
+  μ := μₒ
+  α := αₒ
+  φ := i.φ
+
+instance : CountParts String Float Char where
+  φ _ _ := '?'
+
+instance : CountParts String Char Nat where
+  φ haystack needle := haystack.foldl (fun acc x => acc + if x == needle then 1 else 0) 0
--- Main.lean
+++ Main.lean
@@ -1,10 +1,15 @@
 import FuncDep
 
-def countStrX (xs : String) : FuncDep.instCountPartsString.α :=
+def countStrX (xs : String) : Nat :=
   FuncDep.CountParts.φ xs 'x'
+
+def countNonsense (xs : String) : Char :=
+  FuncDep.CountParts.φ xs 0.3
 
 def main : IO Unit := do
   let nₓ := countStrX "Rhox"
-  -- IO.println s!"{nₓ}"
-  let n₂ := FuncDep.CountParts.φ "Tarmogoyf" 'o'
+  IO.println s!"{nₓ}"
+  -- let n₂ := FuncDep.CountParts.φ "Tarmogoyf" 'o'
   -- IO.println s!"{n₂}"
+  let n₃ := countNonsense "Paradox Engine"
+  IO.println s!"{n₃}"
